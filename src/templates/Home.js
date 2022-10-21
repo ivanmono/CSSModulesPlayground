dc714d60b14a7ca5acaf5c52ff1d08209d4fe6dd
Alen Rajher <arajher@mono.software>
2022-10-21 14:36:03 +0200

    check commit

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -4,19 +4,20 @@ import Rect from '../components/Rect/Rect'
 import Layout from '../components/Layout/Layout'
 
 export default class Home extends React.Component {
-  render() {
-    return (
-        <div>
-            <h1 className="mb-sml-2">CSS Modules playground</h1>
-            <p>Let's play!</p>
+    render() {
+        return (
+            <div>
+                <h1 className="mb-sml-2">CSS Modules playground</h1>
+                <p>Let's play!</p>
 
-            <Layout size={"four"} />
+                <Layout size={"four"} />
 
-            <Rect value={"I am rectangle."} toggle={"is-toggled"} color={"red"} margin={"mb-sml-2"}/>
 
-            <Button type={"primary"} size={"base"} value={"Submit"}/>
-            <Button type={"secondary"} size={"base"} value={"Submit"}/>
-        </div>
-    )
-  }
+                <Rect value={"I am rectangle."} toggle={"is-toggled"} color={"red"} margin={"mb-sml-2"} />
+
+                <Button type={"primary"} size={"base"} value={"Submit"} />
+                <Button type={"secondary"} size={"base"} value={"Submit"} />
+            </div>
+        )
+    }
 }
